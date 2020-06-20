c48d3be28e051855ab55a20eab40ff4050587860
Drin <adrien.bekker@gmail.com>
2020-06-20 18:11:46 -0400

    Main Menu
    Created Main Menu with start and exit buttons.

--- src/App.java
+++ src/App.java
@@ -5,7 +5,10 @@ public class App extends Application {
     @Override
     public void start(Stage primaryStage)
     {
-        System.out.println("HEyyyyyyyy");
+        MainMenu start = new MainMenu(primaryStage);
+        primaryStage.setScene(start.getScene());
+        start.buttonActivation();
+        primaryStage.show();
     }
 
     public static void main(String[] args) 
--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,4 +1,62 @@
-public class MainMenu 
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class MainMenu extends Screen
 {
+    private Button start;
+    private Button exit;
+
+    public MainMenu(Stage stage)
+    {
+        super(stage);
+    }
+
+    public Scene getScene()
+    {
+        VBox vBox = new VBox(20);
+        vBox.setAlignment(Pos.CENTER);
+
+        // Creates labels and buttons
+        Label label = new Label("Surgery Simulator");
+        start = new Button("Start");
+        exit = new Button("Exit");
         
+
+
+        // Adds the objects to the box, then to the scene, and returns it
+        vBox.getChildren().addAll(label, start, exit);
+        Scene scene = new Scene(vBox, 300, 300);
+        return scene;
+    }
+
+    public void buttonActivation()
+    {
+        // When start button is pressed
+        EventHandler<ActionEvent> eventStart = new EventHandler<ActionEvent>() { 
+            public void handle(ActionEvent e) 
+            { 
+                //switchScene();
+            } 
+        };
+
+        start.setOnAction(eventStart);
+
+        // When exit button is pressed
+        EventHandler<ActionEvent> eventExit = new EventHandler<ActionEvent>() { 
+            public void handle(ActionEvent e) 
+            { 
+                System.exit(0);
+            } 
+        };
+
+        exit.setOnAction(eventExit);
+    }
+    
+
 }
